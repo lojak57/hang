e44933e1994cdbb8c1c76e500b2e565eb0ab5f0f
Mitch <mitch.mechelay57@icloud.com>
2025-01-29 21:20:41 -0700

    Explicitly set Node.js version to 18.x

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs18.x',
+			regions: ['iad1']
+		}),
 		alias: {
 			$lib: './src/lib'
 		}
