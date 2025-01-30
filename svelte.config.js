3f24e4788c66dabc17ad30271bf5b3fa84514a47
Mitch <mitch.mechelay57@icloud.com>
2025-01-29 21:25:17 -0700

    Update svelte.config.js to explicitly specify Node.js 18.x runtime

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x',
-			regions: ['iad1']
-		}),
-		alias: {
-			$lib: './src/lib'
-		}
-	}
+			runtime: 'nodejs18.x'
+		})
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
